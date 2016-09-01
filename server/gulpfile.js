e361edf4a65b898f8759ab0d0282a37147f13b26
Deseteral <deseteral@gmail.com>
2016-09-01 19:22:03 +0200

    Build system setup

---
+++ server/gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('build-js', () => gulp
+  .src('src/**/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(babel({
+    presets: ['es2015-node6']
+  }))
+  .pipe(sourcemaps.write(''))
+  .pipe(gulp.dest('build'))
+);
+
+gulp.task('default', ['build-js']);
