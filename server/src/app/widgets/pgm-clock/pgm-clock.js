317ce6d49b88faf36bb0e4bf1d8cf0f42e4aa9ab
Deseteral <deseteral@gmail.com>
2016-09-08 00:16:08 +0200

    Date rendering

--- server/src/app/widgets/pgm-clock/pgm-clock.js
+++ server/src/app/widgets/pgm-clock/pgm-clock.js
@@ -1,15 +1,18 @@
 Polymer({
   is: 'pgm-clock',
   properties: {
-    formattedTime: String
+    formattedTime: String,
+    formattedDate: String
   },
 
   ready() {
     this.formattedTime = this._formatTime();
+    this.formattedDate = this._formatDate();
 
     const CLOCK_REFRESH_INTERVAL = 500;
     setInterval(() => {
       this.formattedTime = this._formatTime();
+      this.formattedDate = this._formatDate();
     }, CLOCK_REFRESH_INTERVAL);
   },
 
@@ -32,7 +35,47 @@ Polymer({
       .reduce((acc, el) => acc + ':' + el);
   },
 
+  _formatDate() {
+    const date = this._getCurrentDate();
+    const monthNames = [
+      'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+      'September', 'October', 'November', 'December'
+    ];
+    const weekDayNames = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+      'Saturday'
+    ];
+
+    // eg. 'Tuesday, 6th September 2016'
+    return [
+      weekDayNames[date.getDay()] + ',',
+      this._ordinal(date.getDate()),
+      monthNames[date.getMonth()],
+      date.getFullYear()
+    ].join(' ');
+  },
+
   _getCurrentDate() {
     return new Date();
+  },
+
+  _ordinal(number) {
+    if (Math.floor(number / 10) === 1) {
+      return number + 'th';
+    }
+
+    if (number % 10 === 1) {
+      return number + 'st';
+    }
+
+    if (number % 10 === 2) {
+      return number + 'nd';
+    }
+
+    if (number % 10 === 3) {
+      return number + 'rd';
+    }
+
+    return number + 'th';
   }
 });
--- server/src/app/widgets/pgm-clock/tests/pgm-clock-spec.js
+++ server/src/app/widgets/pgm-clock/tests/pgm-clock-spec.js
@@ -8,7 +8,7 @@ describe('<pgm-clock>', () => {
     document.querySelector('#fixture').create();
     pgmClock = document.querySelector('pgm-clock');
 
-    const fakeCurrentDate = new Date(2016, 9, 6, 23, 47, 12);
+    const fakeCurrentDate = new Date(2016, 8, 6, 23, 47, 12);
     const fakeCurrentDateFunction = sinon.stub();
     fakeCurrentDateFunction.returns(fakeCurrentDate);
 
@@ -26,6 +26,52 @@ describe('<pgm-clock>', () => {
 
   it('should correctly format time', () => {
     const time = pgmClock._formatTime();
-    time.should.be.eql('23:47:12');
+    time.should.equal('23:47:12');
+  });
+
+  it('should correctly format date', () => {
+    const date = pgmClock._formatDate();
+    date.should.equal('Tuesday, 6th September 2016');
+  });
+
+  it('should format ordinal numbers correctly', () => {
+    const expected = [
+      '',
+      '1st',
+      '2nd',
+      '3rd',
+      '4th',
+      '5th',
+      '6th',
+      '7th',
+      '8th',
+      '9th',
+      '10th',
+      '11th',
+      '12th',
+      '13th',
+      '14th',
+      '15th',
+      '16th',
+      '17th',
+      '18th',
+      '19th',
+      '20th',
+      '21st',
+      '22nd',
+      '23rd',
+      '24th',
+      '25th',
+      '26th',
+      '27th',
+      '28th',
+      '29th',
+      '30th',
+      '31st'
+    ];
+
+    for (let i = 1; i <= 31; i++) {
+      pgmClock._ordinal(i).should.equal(expected[i]);
+    }
   });
 });
