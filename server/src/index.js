2c8b7c6afea993c34a09a42633116f721a03f95e
Deseteral <deseteral@gmail.com>
2016-09-09 00:58:32 +0200

    Fetching new backgrounds every 30 minutes

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,10 @@ const PORT = 3426;
 const subreddits = ['earthporn', 'skyporn'];
 const redditBackgrounds = new RedditBackgrounds(subreddits);
 
+// Refresh every 30 minutes
+const REDDIT_BACKGROUNDS_REFRESH_INTERVAL = 1000 * 60 * 30;
+
+// Server routing
 app.use('/', express.static(nodePath.join(__dirname, 'app')));
 app.use('/widgets', express.static(nodePath.join(__dirname, 'app/widgets')));
 app.use('/bower_components',
@@ -19,17 +23,32 @@ app.get('/background', (req, res) => {
   res.send(url);
 });
 
+// Fetch backgrounds and start webserver
 console.log('Fetching background images from reddit...');
 redditBackgrounds.fetchBackgrounds()
   .then(() => {
-    const backgroundsNumber = redditBackgrounds.backgrounds.length;
-    console.log(`Fetched ${backgroundsNumber} backgrounds from reddit`);
+    handleRedditFetch();
 
     app.listen(PORT, () =>
       console.log(`Prodigium server running on port ${PORT}`)
     );
   })
-  .catch((err) => {
-    console.error(err);
-    console.error('Failed to fetch reddit backgrounds');
-  });
+  .catch(handleRedditFetchError);
+
+// Setup interval for background fetching
+setInterval(() => {
+  console.log('Fetching background images from reddit...');
+  redditBackgrounds.fetchBackgrounds()
+    .then(handleRedditFetch)
+    .catch(handleRedditFetchError);
+}, REDDIT_BACKGROUNDS_REFRESH_INTERVAL);
+
+function handleRedditFetch() {
+  const backgroundsNumber = redditBackgrounds.backgrounds.length;
+  console.log(`Fetched ${backgroundsNumber} backgrounds from reddit`);
+}
+
+function handleRedditFetchError(err) {
+  console.error(err);
+  console.error('Failed to fetch reddit backgrounds');
+}
